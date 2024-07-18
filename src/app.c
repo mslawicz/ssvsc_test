441531a8bef4c040038d0291c37ac4a3830beb75
Marcin Slawicz <marcin.slawicz@etteplan.com>
2024-07-18 10:14:15 +0200

    changing PWM duty

--- src/app.c
+++ src/app.c
@@ -19,6 +19,7 @@
 #include "em_gpio.h"
 #include "pin_config.h"
 #include "sl_simple_led_instances.h"
+#include "sl_pwm_instances.h"
 
 /***************************************************************************//**
  * Initialize application.
@@ -28,6 +29,8 @@ void app_init(void)
   blink_init();
 
   GPIO_PinModeSet(test_out_1_PORT, test_out_1_PIN, gpioModePushPull, 0);
+  sl_pwm_set_duty_cycle(&sl_pwm_pulse_1, 10);
+  sl_pwm_start(&sl_pwm_pulse_1);
 }
 
 /***************************************************************************//**
@@ -35,12 +38,19 @@ void app_init(void)
  ******************************************************************************/
 void app_process_action(void)
 {
+  static uint8_t duty = 10;
   sl_led_state_t led_state_prev = sl_led_get_state(&sl_led_led0);
   blink_process_action();
   if(sl_led_get_state(&sl_led_led0) != led_state_prev)
   {
     // led state changed
     GPIO_PinOutToggle(test_out_1_PORT, test_out_1_PIN);
+    sl_pwm_set_duty_cycle(&sl_pwm_pulse_1, duty);
+    duty += 10;
+    if(duty > 90)
+    {
+      duty = 10;
+    }
   }
 
 }
